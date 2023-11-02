64261f656aa0f127aa31f5748254c7f55cb9bd04
zeeshan3534 <mzeeshankhan3534@gmail.com>
2023-11-02 12:51:22 +0500

    first commit

---
+++ Flash Chat iOS13/Constant.swift
@@ -0,0 +1,9 @@
+//
+//  Constant.swift
+//  Flash Chat iOS13
+//
+//  Created by Zeeshan Khan on 22/09/2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ Flash Chat iOS13/Model/ErrorAlart.swift
@@ -0,0 +1,9 @@
+//
+//  ErrorAlart.swift
+//  Flash Chat iOS13
+//
+//  Created by Zeeshan Khan on 21/09/2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
